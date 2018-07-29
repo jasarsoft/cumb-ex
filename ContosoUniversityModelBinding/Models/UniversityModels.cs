ca2274c5cc8deb193b1169017cd9622631258275
Edin Jašarević <edinjasar14@gmail.com>
2018-07-29 02:02:01 +0200

    model podataka

--- ContosoUniversityModelBinding/Models/UniversityModels.cs
+++ ContosoUniversityModelBinding/Models/UniversityModels.cs
@@ -8,7 +8,9 @@ namespace ContosoUniversityModelBinding.Models
     public class SchoolContext : DbContext
     {
         public DbSet<Student> Students { get; set; }
+
         public DbSet<Enrollment> Enrollments { get; set; }
+
         public DbSet<Course> Courses { get; set; }
     }
 
