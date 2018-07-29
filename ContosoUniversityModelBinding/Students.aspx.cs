9761900ed55efa2aafc7a88cbd75160b74bf2691
Edin Jašarević <edinjasar14@gmail.com>
2018-07-29 16:09:30 +0200

    update i delete opcije na gridview

--- ContosoUniversityModelBinding/Students.aspx.cs
+++ ContosoUniversityModelBinding/Students.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using ContosoUniversityModelBinding.Models;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 
 namespace ContosoUniversityModelBinding
 {
@@ -27,6 +28,49 @@ namespace ContosoUniversityModelBinding
             SchoolContext db = new SchoolContext();
             var query = db.Students.Include(s => s.Enrollments.Select(e => e.Course));
             return query;
+        }
+
+        // The id parameter name should match the DataKeyNames value set on the control
+        public void studentsGrid_UpdateItem(int studentID)
+        {
+            using (SchoolContext db = new SchoolContext())
+            {
+                ContosoUniversityModelBinding.Models.Student item = null;
+                item = db.Students.Find(studentID);
+                // Load the item here, e.g. item = MyDataLayer.Find(id);
+                if (item == null)
+                {
+                    // The item wasn't found
+                    ModelState.AddModelError("", String.Format("Item with id {0} was not found", studentID));
+                    return;
+                }
+                TryUpdateModel(item);
+                if (ModelState.IsValid)
+                {
+                    // Save changes here, e.g. MyDataLayer.SaveChanges();
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        // The id parameter name should match the DataKeyNames value set on the control
+        public void studentsGrid_DeleteItem(int studentID)
+        {
+            using (SchoolContext db = new SchoolContext())
+            {
+                ContosoUniversityModelBinding.Models.Student item = new ContosoUniversityModelBinding.Models.Student() {StudentID = studentID};
+
+                db.Entry(item).State = EntityState.Deleted;
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    ModelState.AddModelError("",
+                      String.Format("Item with id {0} no longer exists in the database.", studentID));
+                }
+            }
         }
     }
 }
